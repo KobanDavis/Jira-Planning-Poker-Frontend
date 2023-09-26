bfe642573f09be722fc5591f3eac8ad2df00f4e2
Koban Davis <koban.davis@mondago.com>
2023-09-26 09:31:54 +0100

    import font using local font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,22 @@ import { AppProps } from 'next/app'
 import { GameProvider } from 'providers/game'
 import { JiraProvider } from 'providers/jira'
 import { ThemeProvider } from 'providers/theme'
+import localFont from 'next/font/local'
 
 import 'styles/globals.css'
+import { useEffect } from 'react'
+
+const satoshi = localFont({
+	src: [
+		{ path: './Satoshi-Variable.woff2', style: 'normal' },
+		{ path: './Satoshi-VariableItalic.woff2', style: 'italic' },
+	],
+})
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
-	const theme = { primary: '#286983', secondary: '#faf4ed' } // && undefined
+	const theme = { primary: '#286983', secondary: '#faf4ed' }
+
+	useEffect(() => document.body.classList.add(satoshi.className), [])
 
 	return (
 		<ThemeProvider initialTheme={theme}>
