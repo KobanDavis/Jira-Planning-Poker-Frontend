0fe98eb43687cc1558d3204f8f64b2f0920ef6c8
Koban Davis <koban.davis@mondago.com>
2023-11-08 12:50:20 +0000

    post check

--- pages/api/auth/exchange.ts
+++ pages/api/auth/exchange.ts
@@ -9,6 +9,8 @@ export interface ErrorResponse {
 
 export default async (req: NextApiRequest, res: NextApiResponse<JiraAuth | ErrorResponse>) => {
 	let code: string
+	console.log(req.method)
+	if (req.method !== 'POST') return res.status(405)
 	if (!req.body.code) {
 		return res.status(400).json({ error: 'Missing `code` property in body.' })
 	} else {
